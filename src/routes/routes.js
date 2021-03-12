a40f4f59dd7a38be0a2869ee3d9590788071e4ec
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-03-11 23:43:08 -0300

    ajustando rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,43 @@
 const { Router } = require('express');
+const app = require('../app');
 
 const router = Router()
 
-router.get('/',(request, response ) => {
+router.get('/courses', (request, response ) => {
 
-    return response.json({message: ' ola mundo '})
+    return response.json([
+        {
+            id: 1,
+            nome: 'Sitemas'
+        },
+        {
+            id: 2,
+            nome: 'Informatica'
+        },
+        {
+            id: 3,
+            nome: 'Administracao'
+        }
+    ])
 
 })
+
+router.post('/courses', (request, response) => {
+    return response.json({ola: true})
+})
+
+router.put('/cousers/:id', (request, response) => {
+return response.json({ola: true})
+})
+
+router.patch('/cousers/:id', (request, response) => {
+    return response.json({ola: true})
+})
+
+router.delete('/cousers/:id', (request, response) => {
+    return response.json({ola: true})
+})
+
+
 
 module.exports = router
